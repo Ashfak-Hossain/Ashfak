5ff85282f6ef86554ce430c541343e4cdd588eda
Berlin <evan1234.ek@gmail.com>
2024-06-29 22:14:48 +0600

    add header

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 
+import Header from '@/components/Header';
+
 import '@/app/globals.css';
 
 const font = Inter({ subsets: ['latin'] });
@@ -19,9 +21,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${font.className} relative bg-gray-50 text-gray-950`}>
+      <body
+        className={`${font.className} relative h-[5000px] bg-gray-50 text-gray-950`}
+      >
         <div className="absolute -top-24 right-44 -z-10 size-[31.25rem] rounded-full bg-[#fbe2e3] blur-[10rem] sm:w-[68.75rem]"></div>
         <div className="absolute -top-4 left-[-35rem] -z-10 h-[31.25rem] w-[50rem] rounded-full bg-[#dbd7fb] blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:-left-60 2xl:-left-20"></div>
+        <Header />
         {children}
       </body>
     </html>
