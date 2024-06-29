27e0ce0c728bf55965b0c7d4173746699bd8a716
Berlin <evan1234.ek@gmail.com>
2024-06-29 21:27:03 +0600

    add static files and data

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,13 @@
-import { Inter } from 'next/font/google';
-import './globals.css';
+import React from 'react';
 import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+
+import '@/app/globals.css';
 
 const font = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Ashfak Hossain',
+  title: 'Ashfak | Software Engineer',
   description:
     'Personal website of Ashfak Hossain, a software engineer. I write about software engineering, programming, and technology and share my thoughts on various topics.',
 };
@@ -17,7 +19,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={font.className}>{children}</body>
+      <body className={`${font.className} relative bg-gray-50 text-gray-950`}>
+        <div className="absolute -top-24 right-44 -z-10 size-[31.25rem] rounded-full bg-[#fbe2e3] blur-[10rem] sm:w-[68.75rem]"></div>
+        <div className="absolute -top-4 left-[-35rem] -z-10 h-[31.25rem] w-[50rem] rounded-full bg-[#dbd7fb] blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:-left-60 2xl:-left-20"></div>
+        {children}
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const page = () => {
-  return <div>page</div>;
+  return <main className="text-4xl font-bold text-cyan-500">page</main>;
 };
 
 export default page;
