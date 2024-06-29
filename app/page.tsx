ce82087215adf3f4258b2e258e3da5d9df585308
Berlin <evan1234.ek@gmail.com>
2024-06-30 01:22:39 +0600

    add section divider

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import Intro from '@/components/Intro';
+import SectionDivider from '@/components/SectionDivider';
 
 const page = () => {
   return (
     <main className="flex flex-col items-center px-4">
       <Intro />
+      <SectionDivider />
     </main>
   );
 };
