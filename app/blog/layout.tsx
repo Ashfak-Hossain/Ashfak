ebac115215d20d39fc94b6b74a06dfd3437ea767
Berlin <evan1234.ek@gmail.com>
2024-07-18 15:54:35 +0600

    routing refactored

--- app/blog/layout.tsx
+++ app/blog/layout.tsx
@@ -1,4 +1,10 @@
 import React from 'react';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Ashfak Hossain | Blog',
+  description: '',
+};
 
 const BlogLayout = () => {
   return <div className="bg-white text-black">BlogLayout</div>;
--- hooks/useSectionInView.ts
+++ hooks/useSectionInView.ts
@@ -1,4 +1,5 @@
 import { useEffect } from 'react';
+import { usePathname } from 'next/navigation';
 import { useInView } from 'react-intersection-observer';
 
 import { useActiveSectionContext } from '@/hooks/useActiveSectionContext';
@@ -10,6 +11,16 @@ export const useSectionInView = (
 ) => {
   const { ref, inView } = useInView({ threshold });
   const { setActiveSection, timeOfLastClick } = useActiveSectionContext();
+  const pathname = usePathname();
+
+  useEffect(() => {
+    if (pathname !== '/blog' && Date.now() - timeOfLastClick > 1000) {
+      const hash = `#${sectionName.toLowerCase()}`;
+      if (window.location.hash !== hash) {
+        window.history.pushState(null, '', hash);
+      }
+    }
+  }, [pathname, inView, sectionName, timeOfLastClick]);
 
   useEffect(() => {
     if (inView && Date.now() - timeOfLastClick > 1000) {
