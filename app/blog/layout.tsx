2bd30b7b1ff05469ef3e3c29f76e70d33d58c9c0
Berlin <evan1234.ek@gmail.com>
2024-07-18 20:00:15 +0600

    refactor: Update BlogLayout component for better performance and responsiveness

--- app/blog/layout.tsx
+++ app/blog/layout.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import { Metadata } from 'next';
 
-export const metadata: Metadata = {
+const data = {
   title: 'Ashfak Hossain | Blog',
-  description: '',
+  description:
+    'I write about competitive programming, software engineering, and other computer science related topics.',
+  url: '/blog',
 };
 
-const BlogLayout = () => {
-  return <div className="bg-white text-black">BlogLayout</div>;
+export const metadata: Metadata = {
+  metadataBase: new URL('https://ashfak.me/blog'),
+  title: data.title,
+  description: data.description,
 };
 
-export default BlogLayout;
+export default function BlogLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <div>{children}</div>;
+}
