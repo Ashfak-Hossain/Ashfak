4a2abac87d5f981f4331e39fa55365f491fae0fe
Berlin <evan1234.ek@gmail.com>
2024-07-28 22:23:01 +0600

    user button refactored

--- components/blog/navbar/user-button.tsx
+++ components/blog/navbar/user-button.tsx
@@ -1,36 +1,52 @@
 import React from 'react';
-import { FaUser } from 'react-icons/fa';
-import { IoExitOutline } from 'react-icons/io5';
+import { LayoutDashboard, LogOut, User } from 'lucide-react';
 
-import { auth } from '@/auth';
 import { LogoutButton } from '@/components/auth/logout-button';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import {
   DropdownMenu,
   DropdownMenuContent,
+  DropdownMenuGroup,
   DropdownMenuItem,
-  // DropdownMenuLabel,
-  // DropdownMenuSeparator,
+  DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
+import { CurrentUser } from '@/lib/auth';
 
 export default async function UserButton() {
-  const session = await auth();
+  const user = await CurrentUser();
+
+  const username = user?.name?.split(' ')[1] || user?.email?.split('@')[0];
 
   return (
     <DropdownMenu>
-      <DropdownMenuTrigger>
+      <DropdownMenuTrigger asChild>
         <Avatar>
-          <AvatarImage src={session?.user.image || ''} alt="user photo" />
+          <AvatarImage src={user?.image || ''} alt="user photo" />
           <AvatarFallback className="bg-sky-500">
-            <FaUser className="text-white" />
+            <User className="text-white" />
           </AvatarFallback>
         </Avatar>
       </DropdownMenuTrigger>
-      <DropdownMenuContent className="w-40" align="end">
+      <DropdownMenuContent className="w-56 p-2 font-medium" align="end">
+        <DropdownMenuItem>
+          <User className="mr-2 size-4" />
+          <span>{username}</span>
+        </DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuGroup>
+          {user?.role === 'ADMIN' && (
+            <DropdownMenuItem>
+              <LayoutDashboard className="mr-2 size-4" />
+              <span>Dashboard</span>
+            </DropdownMenuItem>
+          )}
+          {/* something later like personal notes in dashboard */}
+        </DropdownMenuGroup>
+        <DropdownMenuSeparator />
         <LogoutButton>
           <DropdownMenuItem>
-            <IoExitOutline className="mr-2 size-4" />
+            <LogOut className="mr-2 size-4" />
             Logout
           </DropdownMenuItem>
         </LogoutButton>
