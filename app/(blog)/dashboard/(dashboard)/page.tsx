8fa241bbfb5b75b85fddd8a70ce6615f2b764db4
Berlin <evan1234.ek@gmail.com>
2024-08-19 23:50:32 +0600

    refactor: Remove unused code

--- app/(blog)/dashboard/(dashboard)/page.tsx
+++ app/(blog)/dashboard/(dashboard)/page.tsx
@@ -1,23 +1,7 @@
 import React from 'react';
 
-import { getAllBlogs } from '@/actions/blog/blog.action';
-
 const page = async () => {
-  const blogs = await getAllBlogs();
-
-  return (
-    <div>
-      {blogs.map((blog) => (
-        <div key={blog.id}>
-          <h1>{blog.title}</h1>
-          Tags:
-          {blog.tags.map((tag) => (
-            <span key={tag.id}>{tag.label}</span>
-          ))}
-        </div>
-      ))}
-    </div>
-  );
+  return <div>Hi</div>;
 };
 
 export default page;
